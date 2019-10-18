30498570e2c4f938b4231d20fbcbda3c49142409
Adam Hurm <adamhurm@users.noreply.github.com>
2019-10-18 13:54:10 -0400

    Adam's Tweaks
    + Scan full response instead of only response body
    + Update token on subsequent requests
    
    This extension is forked from Burp's "add-custom-header" repo. I did not realize that 14 days ago the original creator added the "full response body" tweak to the extension.
    
    I also added an IHttpListener so that the token can be updated on subsequent requests.
    
    OLD FLOW:
    Check session is valid by sending current request -> (session is invalid) -> send request(s) to fetch CSRF token -> (Burp updates cookies and extension updates with the fetched CSRF token) -> send updated, valid request
    
    NEW FLOW:
    Check if CSRF token has been fetched -> (CSRF token has been fetched) -> (extension updates with the fetched CSRF token) -> send updated, valid request
    OR
    Check if CSRF token has been fetched -> (CSRF token has NOT been fetched) -> Check session is valid by sending current request -> (session is invalid) -> send request(s) to fetch CSRF token -> (Burp updates cookies and extension uses the fetched CSRF token) -> send updated, valid request

--- burp/BurpExtender.java
+++ burp/BurpExtender.java
@@ -2,6 +2,8 @@ package burp;
 
 import java.awt.Component;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -9,20 +11,25 @@ import javax.swing.JPanel;
 
 import javax.swing.SwingUtilities;
 
-public class BurpExtender implements IBurpExtender, ISessionHandlingAction, ITab {
+public class BurpExtender implements IBurpExtender, ISessionHandlingAction, IHttpListener, ITab {
 
     IExtensionHelpers helpers = null;
     Pattern p;
 
-    static String extensionName = "Add Custom Header";
+    static String extensionName = "CSRF Token Maintainer";
     IBurpExtenderCallbacks callbacks = null;
 
     // some default values
-    final String DEFAULT_HEADER_NAME = "Authorization";
-    final String DEFAULT_HEADER_VALUE_PREFIX = "Bearer ";
-    final String DEFAULT_REGEXP = "access_token\":\"(.*?)\"";
+    final String DEFAULT_HEADER_NAME = "CSRF-Token";
+    final String DEFAULT_HEADER_VALUE_PREFIX = "";
+    final String DEFAULT_REGEXP = "CSRF-Token: (([a-z0-9]{4,12}-?){10})";
     final String DEFAULT_HARDCODED_VALUE = "<insert static JWT token here>";
 
+    String existingToken = null;
+    public static final List<Integer> SYNC_TARGET_TOOL_LIST = Arrays.asList( IBurpExtenderCallbacks.TOOL_REPEATER,
+			IBurpExtenderCallbacks.TOOL_SCANNER, IBurpExtenderCallbacks.TOOL_INTRUDER,
+			IBurpExtenderCallbacks.TOOL_SEQUENCER, IBurpExtenderCallbacks.TOOL_SPIDER );
+    
     private BurpTab tab;
 
     void useRegExp() {
@@ -35,6 +42,8 @@ public class BurpExtender implements IBurpExtender, ISessionHandlingAction, ITab
         callbacks.setExtensionName(extensionName);
         this.helpers = callbacks.getHelpers();
         callbacks.registerSessionHandlingAction(this);
+	    // ADAM: register HTTP Listener to update token for subsequent requests.
+	    callbacks.registerHttpListener(this);
 
         // create our UI
         SwingUtilities.invokeLater(new Runnable() {
@@ -55,16 +64,17 @@ public class BurpExtender implements IBurpExtender, ISessionHandlingAction, ITab
             }
         });
 
-        callbacks.printOutput("Add Header Extension loaded");
+        callbacks.printOutput("CSRF Token Maintainer loaded. -- Based on \"Add Custom Header\". Modified by ahurm.");
     }
 
+    
     // methods below from ISessionHandlingAction
     @Override
     public String getActionName() {
         return extensionName;
     }
 
-    @Override
+    
     public void performAction(IHttpRequestResponse currentRequest,
             IHttpRequestResponse[] macroItems) {
 
@@ -89,18 +99,21 @@ public class BurpExtender implements IBurpExtender, ISessionHandlingAction, ITab
 
             // go through all macros and run the regular expression on their body
             for (int i = 0; i < macroItems.length; i++) {
-                byte[] _responseBody = macroItems[i].getResponse();
-                if (_responseBody == null) return;
-                IResponseInfo macroResponse = helpers.analyzeResponse(_responseBody);
-                if (macroResponse == null ) return;
-                int bodyOffset = macroResponse.getBodyOffset();
-                String responseBody = helpers.bytesToString(_responseBody).substring(bodyOffset);
-                Matcher m = p.matcher(responseBody);
+                byte[] _response = macroItems[i].getResponse();
+                if (_response == null) return;
+                IResponseInfo macroResponse = helpers.analyzeResponse(_response);
+                if (macroResponse == null) return;
+                //ADAM: made changes below to get full response instead of responseBody
+		        //int bodyOffset = macroResponse.getBodyOffset();
+                String response = helpers.bytesToString(_response); //.substring(bodyOffset)
+                Matcher m = p.matcher(response);
                 if (m.find()) {
                     token = m.group(1);
                     if (token != null && token.length() > 0) {
                         // found it
-                        break;
+                        //ADAM: store token for future use
+			            existingToken = token;
+			            break;
                     }
                 }
             }
@@ -138,6 +151,84 @@ public class BurpExtender implements IBurpExtender, ISessionHandlingAction, ITab
         currentRequest.setRequest(message);
     }
     // end ISessionHandlingAction methods
+
+    //method from IHttpListener
+    @Override
+    public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
+        //ADAM: skip token replacement if existingToken has not been set yet
+        if (existingToken == null) return;
+        String token = null;
+	
+        try {
+            if(SYNC_TARGET_TOOL_LIST.contains(toolFlag)){
+            // the below is grabbed from the performAction method
+            if (tab.useHardCoded()) {
+                // token has priority over regexp
+                token = tab.getHardCodedText();
+            } else if (tab.useRegExp()) {
+                String regexp = tab.getRegExpText();
+                try {
+                    p = Pattern.compile(regexp);
+                } catch (PatternSyntaxException e) {
+                    this.callbacks.issueAlert("Syntax error in regular expression (see extension error window)");
+                    callbacks.printError(e.toString());
+                    return;
+                }
+
+                // process when message is request.
+                if (messageIsRequest) {
+                byte[] rawRequest = messageInfo.getRequest();
+                if (rawRequest == null) return;
+                IRequestInfo _request = helpers.analyzeRequest(rawRequest);
+                if (_request == null) return;
+                String request = helpers.bytesToString(rawRequest);
+                Matcher m = p.matcher(request);
+                    if (m.find()) {
+                        token = m.group(1);
+                        if (token != null && token.length() > 0) {
+                            if (token.equals(existingToken)) return;
+                            else token = existingToken;
+                        }
+                    }
+                }
+            } else {
+                // using the 'disable' button
+                return;
+            }  
+
+            if (token == null) {
+                // nothing found: failing silently to avoid performance impact since this is run every request
+                return;
+            }
+
+            String headerName = tab.getHeaderName();
+            String headerValuePrefix = tab.getHeaderValuePrefix();
+
+            IRequestInfo rqInfo = helpers.analyzeRequest(messageInfo);
+            
+            // retrieve all headers
+            ArrayList<String> headers = (ArrayList<String>) rqInfo.getHeaders();
+            for (int i = 0; i < headers.size(); i++) {
+                if (((String) headers.get(i)).startsWith(headerName + ": " + headerValuePrefix)) {
+                    // there could be more than one header like this; remove and continue
+                    headers.remove(i);
+                }
+            }
+            String newHeader = headerName + ": " + headerValuePrefix + token;
+            headers.add(newHeader);
+            callbacks.printOutput("Added header: '" + newHeader + "'");
+
+            String request = new String(messageInfo.getRequest());
+            String messageBody = request.substring(rqInfo.getBodyOffset());
+
+            // rebuild message
+            byte[] message = helpers.buildHttpMessage(headers, messageBody.getBytes());
+            messageInfo.setRequest(message);
+            }
+        } catch (Exception e) {
+            callbacks.printError("An error occurred while attempting to modify this HTTP message: "+e);
+        }
+	}
 
     // ITab methods
     @Override
